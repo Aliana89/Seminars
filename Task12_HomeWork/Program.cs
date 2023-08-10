2f990bd0570fbfa1ac8ba8f24c9c1a6cfb857b4a
Bulatova Aliana <alyanaay@mail.ru>
2023-08-10 17:34:14 +0300

    Task12_HomeWork

---
+++ Task12_HomeWork/Program.cs
@@ -0,0 +1,25 @@
+﻿//Напишите программу , которая принимает на вход
+ //трехзначное число и на выходе показывает вторую цифру этого числа
+ //456->5;
+//782->8;
+//918-1.
+
+
+int ShowSecondDigit(int num)
+{
+    int thirdDigit = num / 10;
+    int secondDigit = thirdDigit % 10;
+    return secondDigit;
+}
+
+Console.WriteLine("Введите трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 100 || number > 999)
+{
+    Console.WriteLine("Неверное число");
+}
+else
+{
+int newNumber = ShowSecondDigit(number);
+Console.WriteLine($"{newNumber}");
+}
