ccfa333abe3d3ad8a085e151c22fa15015e0093e
Bulatova Aliana <alyanaay@mail.ru>
2023-08-10 20:50:39 +0300

    Task14_HomeWork

---
+++ Task14_HomeWork/Program.cs
@@ -0,0 +1,18 @@
+﻿bool CheckWeekend(int dayNum, int dayNum6, int dayNum7)
+{
+    return dayNum % dayNum6 == 0 || dayNum % dayNum7 == 0;
+}
+
+Console.WriteLine("Введите цифру, обозначающую день недели");
+int dayWeek = Convert.ToInt32(Console.ReadLine());
+
+if (dayWeek > 7)
+{
+    Console.WriteLine("Это не день недели");
+}
+else
+{
+bool result = CheckWeekend(dayWeek, 6, 7);
+Console.WriteLine(result ? "Выходной" : "Не выходной");
+}
+
