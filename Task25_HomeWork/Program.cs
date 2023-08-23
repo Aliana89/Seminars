79f6bf80317d611a83ebfc7794a6800a2e095268
Bulatova Aliana <alyanaay@mail.ru>
2023-08-23 21:32:58 +0300

    Task25_HomeWork

---
+++ Task25_HomeWork/Program.cs
@@ -0,0 +1,20 @@
+﻿//Напишите программу, которая 
+//принимает на вход число (N) и выдает 
+//таблицу кубов чисел от 1 до N
+//3 -> 1,8,27
+//5 -> 1,8,27,64,125
+
+void WriteTableСube(int number)
+{
+    int i = 1;
+    while (i <= number)
+
+    {
+        Console.WriteLine($"{i, 3} -> {i*i*i, 6}");
+        i++;
+    }
+}
+Console.Write("Введите число:  ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+WriteTableСube(n);
