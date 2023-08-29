e460aa3da78261bacfd86ca1e6369a0d5920e2e9
Bulatova Aliana <alyanaay@mail.ru>
2023-08-29 20:40:27 +0300

    Palindrome

---
+++ Task24/Program.cs
@@ -0,0 +1,33 @@
+﻿//Задача 24: Напишите программу, которая
+//принимает на вход число (А) и выдаёт сумму чисел
+//от 1 до А.
+//7 -> 28
+//4 -> 10
+//8 -> 36
+
+
+
+int SumNumbers(int number)
+{
+    int sum = 0;
+    for (int i = 1; i <= number; i++)
+    {
+        checked
+        {
+        sum += i; // sum = sum + 1
+        }
+    }
+    return sum;
+}
+
+Console.WriteLine("Введите натуральное число");
+int number = Convert.ToInt32(Console.ReadLine());
+ 
+int sumNumbers = SumNumbers(number);
+if(number < 1)
+{
+    Console.WriteLine("Введено некорректное число");
+    return;
+}
+Console.WriteLine($"Сумма чисел от 1 до {number} = {sumNumbers}");
+
---
+++ Task30/Program.cs
@@ -0,0 +1,45 @@
+﻿
+
+//Задача 30: Напишите программу, которая
+//выводит массив из 8 элементов, заполненный
+//нулями и единицами в случайном порядке.
+//[1,0,1,1,0,1,0,0]
+
+
+//int[] array = new int[8]; // 0 1 2 3 4 5 6 7 
+//array[2] = 34;
+
+//for (int i = 0; i < array.Length; i++)
+//{
+//   array[i] = i * i;
+// Console.Write(array[i] + " ");
+//}
+
+
+
+
+///array[6] = 6456;
+void FillArray(int[] mass)
+{
+    int i = 0;
+    var random = new Random();
+    while (i < mass.Length)
+    {
+        mass[i] = new Random().Next(2);
+        i++;
+    }
+
+}
+void PrintArray(int[] mass)
+{
+    int i = 0;
+    while(i < mass.Length)
+    {
+        Console.Write($"{mass[i]}");
+        i++;
+    }
+}
+
+int[] array = new int[8];
+FillArray(array);
+PrintArray(array);
