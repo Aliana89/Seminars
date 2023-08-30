975139f4aa526922d0115775d012c8d303c313b7
Bulatova Aliana <alyanaay@mail.ru>
2023-08-30 14:24:55 +0300

    ../Task26/Program.cs

--- Task26/Program.cs
+++ Task26/Program.cs
@@ -3,25 +3,21 @@
 //456 -> 3
 //78 -> 2
 //89126 -> 5
-
-
-
-
-
-
-int DigitCount(int num);
+ int DigitCount(int num)
 {
-    int count = 0;
-    if(num == 0) return 1;
+int count = 0;
+if(num == 0) return 1;
 
-    while(num != 0) 
-    {
-        num = num /10;
-        count++;
-    }
-    return count;
+while(num != 0)
+{
+num = num / 10;
+count++;
+}
+return count;
 }
 
- Console.Write("Введите число:");
-int number = Convert.ToInt32(Console.ReadLine);
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
 Console.Write($"Число {number} состоит из {DigitCount(number)} знаков.");
